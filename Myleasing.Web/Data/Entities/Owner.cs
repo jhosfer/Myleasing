ac0f6b26228dd0d71f00dd9f63022e29dc44a62e
Jhosfer Duarte Bustamante <Jduarteb@intergrupo.com>
2020-01-13 16:10:31 -0500

    se llego  hasta el Video 58

---
+++ Myleasing.Web/Data/Entities/Owner.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Myleasing.Web.Data.Entities
+{
+    public class Owner
+
+    {
+        public string Id { get; set; }
+
+        [MaxLength(20, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public string Document { get; set; }
+
+        [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        [Display (Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+       
+        [MaxLength(20, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Display(Name = "Fixed Phone")]
+        public string FixedPhone { get; set; }
+
+       
+        [Display(Name = "Cell Phone")]
+        [MaxLength(20, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        public string CellPhone { get; set; }
+
+        [MaxLength(100, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        public string Address { get; set; }
+
+        public string Fullname => $"{FirstName} {LastName}";
+        public string FullNameWithDocument => $"{FirstName} {LastName} - {Document}";
+
+        public ICollection<Property> Properties { get; set; }
+
+        public ICollection<Contract> Contracts { get; set; }
+
+    }
+}
---
+++ Myleasing.Web/Migrations/20200113151133_InitialDb.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Myleasing.Web.Migrations
+{
+    public partial class InitialDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Owners",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Document = table.Column<string>(maxLength: 30, nullable: false),
+                    FirstName = table.Column<string>(maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(maxLength: 50, nullable: false),
+                    FixedPhone = table.Column<string>(maxLength: 20, nullable: false),
+                    CellPhone = table.Column<string>(maxLength: 20, nullable: false),
+                    Address = table.Column<string>(maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Owners", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Owners");
+        }
+    }
+}

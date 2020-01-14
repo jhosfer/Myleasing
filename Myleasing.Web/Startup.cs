0578424cd6589ba4f3f61fabc7a021fc2648b7c6
Jhosfer Duarte Bustamante <Jduarteb@intergrupo.com>
2020-01-14 16:59:32 -0500

    Crear base de datos y cargar datos. cuando no exista una BD al principio

--- Myleasing.Web/Startup.cs
+++ Myleasing.Web/Startup.cs
@@ -38,7 +38,7 @@ namespace Myleasing.Web
                 cfg.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
             });
 
-
+            services.AddTransient<SeedDb>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
